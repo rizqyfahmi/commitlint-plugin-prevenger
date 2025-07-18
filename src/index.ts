4e2ffe8bcc2a8ed545204f6dad14a634fd9ba0a4
rizqyfahmi <rizqyfahmi@yahoo.com>
2025-07-18 11:37:01 +0700

    fix(config): enhance package.json and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { default as scopePattern } from "./rules/scope-pattern";
+import { default as scopePattern } from "./rules/scope-pattern/index.js";
 
 export const rules = {
     'scope-pattern': scopePattern
