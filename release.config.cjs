5b678cad0a9e45e800ad4a05b5ed9d986d65d312
rizqyfahmi <rizqyfahmi@yahoo.com>
2025-07-18 10:44:57 +0700

    chore(relase): Enhance trigger release

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
     branches: ["master"],
     plugins: [
-        "@semantic-release/commit-analyzer",
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    { type: "ci", release: "patch" }
+                ]
+            }
+        ],
         "@semantic-release/release-notes-generator",
         [
             "@semantic-release/changelog",
